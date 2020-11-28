cc0c377a530aa2af272dd6098ed1b74af9f57b64
jiangwc <jiangweic24@163.com>
2020-11-28 15:18:56 +0800

    本地库 commit test

--- src/main/com/jwc/Test.java
+++ src/main/com/jwc/Test.java
@@ -3,9 +3,10 @@ package main.com.jwc;
 public class Test {
 
         public static void main(String[] args) {
-            System.out.println("test");
+            System.out.println("test22222");
             System.out.println("test2");
             System.out.println("test");
+            System.out.println();
             System.out.println();
 
         }
