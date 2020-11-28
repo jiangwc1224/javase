3065e1def143d937bddd21dff96e17f0571ccf13
jiangwc <jiangweic24@163.com>
2020-11-28 15:17:36 +0800

    本地库 commit test

---
+++ src/main/com/jwc/Test.java
@@ -0,0 +1,23 @@
+package main.com.jwc;
+
+public class Test {
+
+        public static void main(String[] args) {
+            System.out.println("test");
+            System.out.println("test2");
+            System.out.println("test");
+            System.out.println();
+
+        }
+
+        private void add(){
+//
+        }
+
+
+
+
+        private void sub(){
+
+    }
+}
---
+++ src/main/com/jwc/Test2.java
@@ -0,0 +1,13 @@
+package main.com.jwc;
+
+/**
+ * @Auther: jiangwc
+ * @Date: 2020/11/28 - 11 - 28 - 15:14
+ * @Description: main.com.jwc
+ * @version: 1.0
+ */
+public class Test2 {
+    public static void main(String[] args) {
+
+    }
+}
